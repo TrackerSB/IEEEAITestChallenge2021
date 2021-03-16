f9e1039815a579cf3a7e786f4457f299e82770ee
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-03-16 16:05:28 +0100

    tc6: Add option for specifying initial ego car distance

--- playground/lgsvl/tc6/main.py
+++ playground/lgsvl/tc6/main.py
@@ -1,11 +1,12 @@
 from collections import namedtuple
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 from lgsvl import AgentState, WalkWaypoint
 from lgsvl.geometry import Spawn, Transform, Vector
 
 # Test case configurable settings
-EGO_SPEED: float = 50.0  # km/h
+EGO_SPEED: float = 50.0  # in km/h
+EGO_DISTANCE: Optional[float] = None  # in m: None --> Calculate a distance which enforces a crash with the pedestrian
 
 # Test case fixed settings
 MAP_NAME: str = "San Francisco"
@@ -38,10 +39,14 @@ def _generate_initial_ego_state() -> Tuple[AgentState, float]:
     from common.scene import generate_initial_state
 
     pedestrian_crash_distance = (PEDESTRIAN_SPAWN.position - PEDESTRIAN_CRASH_POS).magnitude()
-    time_to_crash = pedestrian_crash_distance / (PEDESTRIAN_SPEED / 3.6)  # in seconds
-    ego_crash_distance = (EGO_SPEED / 3.6) * time_to_crash
+    if EGO_DISTANCE is None:
+        time_to_crash_point = pedestrian_crash_distance / (PEDESTRIAN_SPEED / 3.6)  # in seconds
+        ego_crash_distance = (EGO_SPEED / 3.6) * time_to_crash_point
+    else:
+        ego_crash_distance = EGO_DISTANCE
+        time_to_crash_point = ego_crash_distance / (EGO_SPEED / 3.6)
     ego_spawn = Spawn(Transform(position=EGO_CRASH_POS + Vector(0, 0, ego_crash_distance), rotation=EGO_ORIENTATION))
-    return generate_initial_state(ego_spawn, EGO_SPEED), time_to_crash
+    return generate_initial_state(ego_spawn, EGO_SPEED), time_to_crash_point
 
 
 def _main() -> None:
@@ -52,7 +57,7 @@ def _main() -> None:
     sim = connect_simulation("127.0.0.1", 8181)
     load_scene(sim, MAP_NAME)
 
-    initial_ego_state, time_to_crash = _generate_initial_ego_state()
+    initial_ego_state, time_to_crash_point = _generate_initial_ego_state()
     ego = load_ego(sim, "Jaguar2015XE (Apollo 5.0, many sensors)", initial_ego_state)
 
     initial_pedestrian_state = _generate_initial_pedestrian_state()
@@ -67,7 +72,7 @@ def _main() -> None:
 
     detect_collisions(sim, _on_collision)
 
-    sim.run(time_to_crash * 1.5)
+    sim.run(time_to_crash_point * 2)
 
     print("Successful" if test_result.successful else "Failed")
 
