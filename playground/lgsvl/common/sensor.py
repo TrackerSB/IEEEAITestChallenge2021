14e5bbd66b458535174c52ee633e51bea85a2c0f
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-04-02 10:14:31 +0200

    tc6: Introduce logging

--- playground/lgsvl/common/sensor.py
+++ playground/lgsvl/common/sensor.py
@@ -4,13 +4,14 @@ from lgsvl import EgoVehicle, Sensor
 
 
 def print_sensor_states(ego: EgoVehicle) -> None:
+    import logging
     sensors = ego.get_sensors()
     for sensor in sensors:
-        print("{} ({}): {}".format(sensor.name, type(sensor), "Enabled" if sensor.enabled else "Disabled"))
+        logging.info("{} ({}): {}".format(sensor.name, type(sensor), "Enabled" if sensor.enabled else "Disabled"))
 
 
 def get_sensor(ego: EgoVehicle, sensor_name: str) -> Optional[Sensor]:
-    from warnings import warn
+    import logging
     sensors = ego.get_sensors()
     for sensor in sensors:
         if sensor.name == sensor_name:
@@ -20,6 +21,6 @@ def get_sensor(ego: EgoVehicle, sensor_name: str) -> Optional[Sensor]:
         desired_sensor = None
 
     if desired_sensor and not desired_sensor.enabled:
-        warn("Found sensor {} but it's disabled" % sensor_name)
+        logging.warning("Found sensor {} but it's disabled" % sensor_name)
 
     return desired_sensor
