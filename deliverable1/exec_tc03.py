de7f76902abaf9355eb677f82f3bd171ae91ba82
harvey <me@ngvuong.com>
2021-04-30 12:12:56 +0200

    Add script to exec single TC

---
+++ deliverable1/exec_tc03.py
@@ -0,0 +1,28 @@
+from unittest import TextTestRunner
+from unittest.suite import TestSuite
+from unittest.loader import TestLoader
+from test_case_03 import TestCase03
+
+
+if __name__ == "__main__":
+    suite = TestSuite()
+    loader = TestLoader()
+    suite.addTests(loader.loadTestsFromTestCase(TestCase03))
+
+    # run the suite
+    debug_enabled = False
+    if debug_enabled:
+        """
+        NOTE Using buffer=False and failfast=True together ensures that any exception occurring during test runs is printed
+        to stderr immediately instead of being hold back until all test cases finished.
+        However at least failfast has to be disabled if test cases contain tests that are expected to fail.
+        """
+        failfast = True
+        buffer = False
+    else:
+        failfast = False
+        buffer = True
+    runner = TextTestRunner(failfast=failfast, buffer=buffer, verbosity=2)
+    runner.run(suite)
+
+
---
+++ deliverable1/exec_tc06.py
@@ -0,0 +1,26 @@
+from unittest import TextTestRunner
+from unittest.suite import TestSuite
+from unittest.loader import TestLoader
+from test_case_06 import TestCase06
+
+if __name__ == "__main__":
+    suite = TestSuite()
+    loader = TestLoader()
+    suite.addTests(loader.loadTestsFromTestCase(TestCase06))
+
+    # run the suite
+    debug_enabled = False
+    if debug_enabled:
+        """
+        NOTE Using buffer=False and failfast=True together ensures that any exception occurring during test runs is printed
+        to stderr immediately instead of being hold back until all test cases finished.
+        However at least failfast has to be disabled if test cases contain tests that are expected to fail.
+        """
+        failfast = True
+        buffer = False
+    else:
+        failfast = False
+        buffer = True
+    runner = TextTestRunner(failfast=failfast, buffer=buffer, verbosity=2)
+    runner.run(suite)
+
