4ae31379c079a89bb684e6a388d346d1cca5c790
Harvey <me@ngvuong.com>
2021-07-14 17:03:25 +0200

    Filter argument

--- playground/lgsvl/buildAndAnalyzeLanelets/runner.py
+++ playground/lgsvl/buildAndAnalyzeLanelets/runner.py
@@ -17,13 +17,18 @@ RIGHT = 1.9
 @click.argument('map_file', type=click.Path(exists=True))
 @click.argument('before_junction', nargs=1)
 @click.argument('after_junction', nargs=1)
-def generate_all_paths(map_file, before_junction, after_junction):
+@click.argument('filter', nargs=-1)
+def generate_all_paths(map_file, before_junction, after_junction, filter):
     map_list = map_file.split('/')
     mmap = {
         "name": map_list[len(map_list) - 1].split('.')[0],
         "path": map_file
     }
     expm = Experiment(mmap=mmap, name="Generate All Possible Paths", plan=StraightModel)
+    if filter != () and filter[0] == "distance":
+        expm.set_filter(Filter.compare_distance)
+    if filter != () and filter[0] == "feature":
+        expm.set_filter(Filter.compare_feature)
     expm.generate_data_paths(float(before_junction), float(after_junction))
 
 
@@ -32,13 +37,18 @@ def generate_all_paths(map_file, before_junction, after_junction):
 @click.argument('before_junction', nargs=1)
 @click.argument('after_junction', nargs=1)
 @click.argument('parking_distance', nargs=1)
-def generate_all_paths_with_parking(map_file, before_junction, after_junction, parking_distance):
+@click.argument('filter', nargs=-1)
+def generate_all_paths_with_parking(map_file, before_junction, after_junction, parking_distance, filter):
     map_list = map_file.split('/')
     mmap = {
         "name": map_list[len(map_list) - 1].split('.')[0],
         "path": map_file
     }
     expm = Experiment(mmap=mmap, name="Generate All Possible Paths", plan=ParkingModel)
+    if filter != () and filter[0] == "distance":
+        expm.set_filter(Filter.compare_distance)
+    if filter != () and filter[0] == "feature":
+        expm.set_filter(Filter.compare_feature)
     expm.generate_data_paths(float(before_junction), float(after_junction), float(parking_distance))
 
 
